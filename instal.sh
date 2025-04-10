9ff225c63ee202c00ede7432481d7792082c6721
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2025-04-10 22:09:27 +0700

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "[*] Menginstal dependensi yang diperlukan..."
+
+daftar_dependensi=(
+        "python3"
+        "python3-colorama"
+        "john"
+        "john-data"
+        "poppler-utils"
+)
+
+apt-get update
+
+for dependensi in "${daftar_dependensi[@]}"; do
+        apt-get install "${dependensi}"
+done
+
+echo "[+] Selesai."
